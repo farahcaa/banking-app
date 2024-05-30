947b9575f1cde51e734b1d5bb8c6e1c13f09cc78
Chris Farah <farahca@mail.uc.edu>
2024-05-30 19:20:58 -0400

    did work

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Chris" };
+  const loggedIn = {
+    firstName: "Chris",
+    lastName: "Farah",
+    email: "farahca@mail.uc.edu",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRASNACRION
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, {}]}
+      />
     </section>
   );
 };
